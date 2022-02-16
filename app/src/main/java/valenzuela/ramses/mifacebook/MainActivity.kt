f6d2c985754b8459e0aabfac58b6c3d49fcc2df8
RamsesValenzuela <jesusramsesalmada@gmail.com>
2022-02-15 21:30:41 -0700

    Maquetacion de Facebook\

---
+++ app/src/main/java/valenzuela/ramses/mifacebook/MainActivity.kt
@@ -0,0 +1,11 @@
+package valenzuela.ramses.mifacebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inicio_face)
+    }
+}
